1f8c5ae1cedb9d79ff22a557aa7aaca07001a50e
erikjvandenberg <e.j.vandenberg@gmail.com>
2017-09-16 10:18:53 +0200

    Add project files.

---
+++ EcstaticDanceDelft/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EcstaticDanceDelft
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ EcstaticDanceDelft/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcstaticDanceDelft.MvcApplication" Language="C#" %>
